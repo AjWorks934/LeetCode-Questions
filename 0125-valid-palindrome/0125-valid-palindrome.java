9385c3373bd294918658a2d5755be0e4038892c3
AjWorks934 <141341722+AjWorks934@users.noreply.github.com>
2024-08-26 17:49:21 +0530

    Time: 123 ms (11.98%), Space: 46 MB (9.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,24 +2,49 @@ import java.util.*;
 class Solution {
     public boolean isPalindrome(String s) {
         String str=s.toLowerCase();
-        int i=0;int j=str.length()-1; boolean flag =true;
-        while(i<j)
+      /*  if((str.charAt(0)<97 && (str.charAt(0)<48 || str.charAt(0)>57 ) ) || str.charAt(0)>122)
+            {
+                if(str.charAt(0)==s.charAt(str.length()-1))
+            { 
+        String s1="";
+        for(int i=0;i<str.length();i++)
         {
-            while(i<j && !Character.isLetterOrDigit(str.charAt(i)))
-            i++;
-            while(i<j && !Character.isLetterOrDigit(str.charAt(j)))
-            j--;
-            if(str.charAt(i)!=str.charAt(j)){
-             
-                flag=false;
-            
-            break;
-            }
-               
-                i++;
-                j--;
-               
+        if((str.charAt(i)>=97 && str.charAt(i)<=122 )||(str.charAt(i)>=48 && str.charAt(i)<=57 ))
+        {
+            s1= s1+str.charAt(i);
         }
-        return flag; 
+        }
+        StringBuilder str1= new StringBuilder();
+        str1.append(s1);
+        str1.reverse();
+        String str2=str1.toString();
+        System.out.println(str2+" "+s1);
+        if(str2.equals(s1))
+        return true;
+        else
+        return false;
+}else 
+return false;
+            
+            }
+else 
+            {*/
+               
+        String s1="";
+        for(int i=0;i<str.length();i++)
+        {
+        if((str.charAt(i)>=97 && str.charAt(i)<=122 )||(str.charAt(i)>=48 && str.charAt(i)<=57 ))
+        {
+            s1= s1+str.charAt(i);
+        }
+        }
+        StringBuilder str1= new StringBuilder();
+        str1.append(s1);
+        str1.reverse();
+        String str2=str1.toString();
+        if(str2.equals(s1))
+        return true;
+        else
+        return false; 
             }
 }
