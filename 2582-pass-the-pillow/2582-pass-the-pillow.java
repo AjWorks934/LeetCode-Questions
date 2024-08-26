245a860f63a9863e79d3552fd24d0a874fd98d98
AjWorks934 <141341722+AjWorks934@users.noreply.github.com>
2024-08-26 17:56:47 +0530

    Time: 1 ms (16.97%), Space: 40.2 MB (48.9%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.java
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -1,13 +1,18 @@
+import java.util.*;
 class Solution {
     public int passThePillow(int n, int time) {
-        int completed = time / (n-1);
-        int remaining = time % (n-1);
-        int ans = 0;
-        if (completed % 2 != 0) {
-            ans = n - remaining;
-        } else {
-            ans = remaining + 1;
-        }
-        return ans;
+       int d=1;
+       int p=1;
+       while(time>0)
+       {
+        if(p>=n||p==-1)
+            d=-1;
+        else
+            d=1;
+        p=p*d+1;
+        //System.out.println(p+"->");
+        time--;
+       }
+       return Math.abs(p);
     }
 }
