cc5ba9bacb74648e3390a0422e20d7f38aa51ae8
AjWorks934 <141341722+AjWorks934@users.noreply.github.com>
2024-08-29 10:34:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        int x=n;int bitm=1;
+        while(n!=1){
+             n=n>>1;
+             System.out.println(n+" @");
+            bitm=bitm<<1;
+            System.out.println(bitm+" ,");
+            bitm=bitm|1;
+             System.out.println(bitm+" ,");
+           
+             
+        }
+        int c=~x;
+        int ans=c&bitm;
+        return ans;
+    }
+}
