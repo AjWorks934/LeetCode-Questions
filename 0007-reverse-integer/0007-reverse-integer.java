92b950eb1b7a1a293b495644453fa2fda93ad14e
AjWorks934 <141341722+AjWorks934@users.noreply.github.com>
2024-08-29 11:43:39 +0530

    Time: 0 ms (100%), Space: 40.8 MB (43.72%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,6 +2,8 @@ class Solution {
     public int reverse(int x) {
         int n=x;long rev=0;
       while(n!=0){
+       // int d=;
+       // if(d!=0)
         rev=rev*10+n%10;
         n=n/10;
       } 
