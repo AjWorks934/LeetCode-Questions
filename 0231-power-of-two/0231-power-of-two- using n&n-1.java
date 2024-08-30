9b54e8f9908ca3190f130d493fdb8172ed749b05
AjWorks934 <141341722+AjWorks934@users.noreply.github.com>
2024-08-30 18:44:59 +0530

    0231-power-of-two- using n&n-1.java

--- 0231-power-of-two/0231-power-of-two- using n&n-1.java
+++ 0231-power-of-two/0231-power-of-two- using n&n-1.java
@@ -1,15 +1,8 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        int c=0;
-    while(n>=1){
-        if((n&1)==1)
-        c++;
-       // System.out.println(c);
-        n=n>>1;
-    }
-   if(c==1)
-   return true;
-   else
-   return false; 
+            if((n&n-1)==0)
+        return true;
+        else
+        return false;
 }
 }
