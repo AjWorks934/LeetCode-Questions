8312801a488ac4ca84f215ea32d9d2a5a74639c4
Aayushi Jain <141341722+AjWorks934@users.noreply.github.com>
2024-09-18 23:17:35 +0530

    Time: 21 ms (11.51%), Space: 41.5 MB (7.05%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -3,7 +3,7 @@ class Solution {
         int x=n;int bitm=1;
         if(n==0)
         return 1;
-        while(n!=1){
+        while(n!=1){//creating bitmask i.e counting no. of bits upto the last 1 in input and creating a bitmask of that length with 1s.
              n=n>>1;
              System.out.println(n+" @");
             bitm=bitm<<1;
@@ -14,7 +14,7 @@ class Solution {
              
         }
         int c=~x;
-        int ans=c&bitm;
+        int ans=c&bitm;//extracting that length ans complement
         return ans;
     }
 }
