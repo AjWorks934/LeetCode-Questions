915f0e3708ff2b888b10abceef01702bdb6817f3
Aayushi Jain <141341722+AjWorks934@users.noreply.github.com>
2024-12-20 23:02:31 +0530

    Time: 71 ms (7.64%), Space: 46.4 MB (91.59%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -6,22 +6,26 @@ class Solution {
         }
         int arr[][]=new int[s.length()+1][s2.length()+1];
         for (int[] row: arr)
-        Arrays.fill(row,-1);
+        Arrays.fill(row,0);
         int ll=LCSTD(s,s.length(),s2,s2.length(),arr);
         return ll;
     }
-        public static int LCSTD(String s1,int i,String s2,int j,int arr[][]){
-        if(i==0||j==0){
-            return 0;
-        }
-        if(arr[i][j]!=-1)
-            return arr[i][j];
-        else{
-            if(s1.charAt(i-1)==s2.charAt(j-1))
-                arr[i][j]= 1+LCSTD(s1,i-1,s2,j-1,arr);
-            else
-                arr[i][j]= Math.max(LCSTD(s1,i,s2,j-1,arr),LCSTD(s1,i-1,s2,j,arr));
-            return arr[i][j];
-        }
+        public static int LCSTD(String s1,int n,String s2,int m,int arr[][])
+        {
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=m;j++){
+                    if(s1.charAt(i-1)==s2.charAt(j-1))
+                        arr[i][j]= 1+arr[i-1][j-1];
+                    else
+                        arr[i][j]= Math.max(arr[i][j-1],arr[i-1][j]);
+                }
+            }
+       // for(int i=0;i<=n;i++){
+           //     for(int j=0;j<=m;j++){
+            //        System.out.print(arr[i][j]+" ");
+            //    }
+            //    System.out.println();
+       // }
+        return arr[n][m];
     }
 }
