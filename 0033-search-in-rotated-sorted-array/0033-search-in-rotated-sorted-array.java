544e16b2ce4bfc33c7e27fc39d21f03ecf5c9b2d
Aayushi Jain <141341722+AjWorks934@users.noreply.github.com>
2025-03-25 12:02:27 +0530

    Time: 0 ms (100%), Space: 42.4 MB (19.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
         int low = 0, high = nums.length - 1;
-
         while (low <= high) {
             int mid = (low + high) / 2;
 
             if (nums[mid] == target) {
                 return mid;
             }
-
-            if (nums[low] <= nums[mid]) {
+            if (nums[low] <= nums[mid]) {//means mid lies in the rotated aage shifted vla part
                 if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
+                    high = mid - 1;//agr uss half mr target exist krta hai 
+                } else {//if not move to next half
                     low = mid + 1;
                 }
             } else {
                 if (nums[mid] < target && target <= nums[high]) {
+                    //means mid lies in the rotated peeche vla part
                     low = mid + 1;
                 } else {
                     high = mid - 1;
                 }
             }
         }
-
         return -1;
     }
 }
