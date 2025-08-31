879019c7940407c3c622d7fddacc4c53b121abeb
Aayushi Jain <141341722+AjWorks934@users.noreply.github.com>
2025-08-31 21:14:01 +0530

    Time: 1 ms (61.53%), Space: 46.1 MB (81.99%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,25 +1,30 @@
 class Solution {
-    public int trap(int[] height) {
-        int l = 0, r = height.length - 1;
-        int lm = height[0], rm = height[height.length - 1];
-        int water = 0;
-        while (l < r) {
-            if (lm < rm) {
-                l++;
-                if (lm < height[l]) {
-                    lm = height[l];
-                } else {
-                    water += lm - height[l];
-                }
-            } else {
-                r--;
-                if (rm < height[r]) {
-                    rm = height[r];
-                } else {
-                    water += rm - height[r];
-                }
-            }
+    public int trap(int[] arr) {
+        int n = arr.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int res = 0;
+
+        // fill left array
+        left[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], arr[i]);
         }
-        return water;
+
+        // fill right array
+        right[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = Math.max(right[i + 1], arr[i]);
+        }
+
+        // calculate the accumulated water element by element
+        for (int i = 0; i < n - 1; i++) {
+            int minOf2 = Math.min(left[i], right[i]);
+            res += minOf2 - arr[i];
+        }
+
+        return res;
     }
-}
+
+    }
