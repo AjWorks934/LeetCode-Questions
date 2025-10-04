51ba331595f46ddc5b8dae509f02c01d97971ccd
Aayushi Jain <141341722+AjWorks934@users.noreply.github.com>
2025-10-04 12:52:54 +0530

    Time: 20 ms (5.27%), Space: 61 MB (36.71%) - LeetHub

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -3,20 +3,47 @@ class Solution {
       long sum1=0; long sum2=0; int z1=0,z2=0; long major=0;
       for(int i=0;i<nums1.length;i++){
         sum1+=nums1[i];
-        if(nums1[i]==0){
+        if(nums1[i]==0)
         z1++;
-        sum1++;
       }  
-      }
+      System.out.println(sum1+"s1");
+      System.out.println(z1+"z1");
       for(int j=0;j<nums2.length;j++){
         sum2+=nums2[j];
-        if(nums2[j]==0){
+        if(nums2[j]==0)
         z2++;
-        sum2++;
-        }
       }
-    if((sum1>sum2 && z2==0)|| (sum2>sum1 && z1==0))
-    return -1;
-    return Math.max(sum1,sum2);
+      System.out.println(sum2+"s2");
+      System.out.println(z2+"z2");
+
+      if(sum1==sum2){
+        if((z1==0 && z2>0)||(z1>0 &&z2==0)){
+            return -1;
+        }
+        long m1=sum1+z1;
+        long m2=sum2+z2;
+        return m1>m2?m1:m2;
+      }
+      else
+      {
+        if(z1==0 && z2==0)
+            return -1;
+        else if(z1==0 && z2>0)
+        {
+            long m2=sum2+z2;
+            if(sum1<m2)
+            return -1;
+        }
+        else if(z2==0 && z1>0)
+        {
+            long m1=sum1+z1;
+            if(sum2<m1)
+            return -1;
+        }
+        
+        long m1=sum1+z1;
+        long m2=sum2+z2;
+        return m1>m2?m1:m2;
+      }
     }
 }
